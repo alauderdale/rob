babd7f993d749340deef74906432cb4fd41d21e5
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2017-02-25 00:57:00 -0800

    firsty

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
 
+// init Masonry
+var $grid = $('.grid').masonry({
+      // set itemSelector so .grid-sizer is not used in layout
+      itemSelector: '.grid-item',
+      // use element for option
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
+
+
+
 	 //fancybox
-	$('.fancybox').fancybox();
+	$('.fancybox').fancybox({
+        padding: 0
+    });
 	
 	$('.fb-video').fancybox({
         padding: 0,
